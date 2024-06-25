819378d9cd61ee86dc549afef2b4bdc22dc591d2
paingsoeko <social.kopaing@gmail.com>
2024-06-25 19:09:14 +0700

    first commit

---
+++ vendor/akaunting/laravel-money/src/Casts/CurrencyCast.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Akaunting\Money\Casts;
+
+use Akaunting\Money\Currency;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use UnexpectedValueException;
+
+/**
+ * @template TGet
+ * @template TSet
+ */
+class CurrencyCast implements CastsAttributes
+{
+    public function get($model, string $key, $value, array $attributes): Currency
+    {
+        if (! is_string($value)) {
+            throw new UnexpectedValueException;
+        }
+
+        return new Currency($value);
+    }
+
+    public function set($model, string $key, $value, array $attributes): string
+    {
+        if (! $value instanceof Currency) {
+            throw new UnexpectedValueException;
+        }
+
+        return $value->getCurrency();
+    }
+}
